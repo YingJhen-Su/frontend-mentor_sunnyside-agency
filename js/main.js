80a175acb5fb0e5291821693f89eb50f28473c69
YingJhen-Su <clearsky0330@gmail.com>
2023-12-08 16:42:29 +0800

    feat: try to add animated menu button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const app = () => {
     const menuBtn = document.querySelector('#menu__btn');
     const menuMobile = document.querySelector('#menu__mobile');
+    const menuBar = document.querySelector('#menu__bar');
 
     const menuToggle = () => {
         menuMobile.classList.toggle('active');
+        menuBar.classList.toggle('toggle');
     }
     
     menuBtn.addEventListener('click', menuToggle);
